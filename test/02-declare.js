c0d9c72b18276d59661d090da4beddbad7e5a18c
Konstantin S. Uvarin <khedin@gmail.com>
2024-07-19 01:13:34 +0200

    [api] Remove abitily to save via parse, let the user to in instead

--- test/02-declare.js
+++ test/02-declare.js
@@ -4,7 +4,7 @@ const { SKI } = require('../index');
 describe( 'SKI', () => {
     it('can declare terms', done => {
         const ski = new SKI;
-        const impl = ski.parse('sub = S(K(SI))K');
+        ski.add('sub', 'S(K(SI))K');
 
         const term = ski.parse('sub x y');
         const result = term.run();
