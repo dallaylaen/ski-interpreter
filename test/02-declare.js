560333e03006938717deeb96af41b3c503a74d0e
Konstantin S. Uvarin <khedin@gmail.com>
2024-07-20 00:45:42 +0200

    [api] Add list() for known terms inspection, add Alias AST node type

--- test/02-declare.js
+++ test/02-declare.js
@@ -11,6 +11,20 @@ describe( 'SKI', () => {
 
         expect( ''+result).to.equal('y(x)');
 
+        // console.log(ski.list());
+
         done();
-    })
+    });
+
+    it('does not overwrite read-only data', done => {
+        const ski = new SKI;
+        ski.add('sub', 'S', 'just an alias');
+
+        const known = ski.list();
+
+        expect (known.S.note).to.match(/S.*->/);
+        expect (known.sub.note).to.equal('just an alias');
+
+        done();
+    });
 });
