8c8706c5991ec38194a17993f2090fe1a2e3e002
Konstantin S. Uvarin <khedin@gmail.com>
2024-07-27 11:22:13 +0200

    [test] Try to make benchmark somewhat more precise

--- example/speed.js
+++ example/speed.js
@@ -1,18 +1,21 @@
 const { SKI } = require ('../index');
 
-
 const ski = new SKI();
 ski.add('inc', 'S(S(K(S))(K))');
 ski.add('n2', 'inc I');
 
-const expr = ski.parse('inc (n2 n2) (n2 n2) x y');
-const depth = 1024;
+const expr = ski.parse('(inc n2 n2 inc n2) n2 x y');
+const depth = 2**10;
+const expect = 'x('.repeat(depth)+'y'+')'.repeat(depth);
+
+const iter = 20;
 
 const t0 = new Date();
-const result = expr.run(10000000);
+for (let i = 0; i < iter; i++) {
+    const result = expr.run(10000000);
+    if (''+result !== expect)
+        console.log('warning: unexpected output: \n'+result);
+}
 const elapsed = new Date() - t0;
 
-if (''+result !== 'x('.repeat(depth)+'y'+')'.repeat(depth))
-    console.log('warning: unexpected output: \n'+result);
-
-console.log("time elapsed: "+elapsed+'ms');
+console.log("time per iteration: "+(elapsed / iter)+'ms');
