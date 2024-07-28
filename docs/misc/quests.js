61f1ba56abd355560fb2a14cbbd253bedb2aefc8
Konstantin S. Uvarin <khedin@gmail.com>
2024-07-28 03:48:58 +0200

    [quests] More quests

--- docs/misc/quests.js
+++ docs/misc/quests.js
@@ -1,8 +1,20 @@
 function getQuests () {
     return [
         [
-            {title: "swap"},
+            {title: 'Uroboros', descr: 'Make a combinator that applies its argument to itself: x -> x x'},
+            ['x x', 'x'],
+        ],
+        [
+            {title: 'Mirror image', descr: 'Make a combinator that swaps its two arguments, that is, (x y) -> y x'},
             [ 'y x', 'x', 'y'],
+        ],
+        [
+            {
+                title: 'Lost identity',
+                descr: 'Someone has dropped the I combinator from your interpreter. You have to rebuild it from the remaining two!',
+                allow: 'SK',
+            },
+            [ 'x', 'x'],
         ],
         [
             {title: "if (then) (else) (true|false)"},
