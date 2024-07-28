e260876c6549efdcb3c57faa9b672aa4d05d407b
Konstantin S. Uvarin <khedin@gmail.com>
2024-07-28 02:05:52 +0200

    [quest] Add quest file for quest mode

---
+++ docs/misc/quests.js
@@ -0,0 +1,13 @@
+function getQuests () {
+    return [
+        [
+            {title: "swap"},
+            [ 'y x', 'x', 'y'],
+        ],
+        [
+            {title: "if (then) (else) (true|false)"},
+            [ 'x', 'x', 'y', 'K'],
+            [ 'y', 'x', 'y', 'KI'],
+        ],
+    ]
+}
