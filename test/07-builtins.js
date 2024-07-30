76fc0894db8e5d63006d2705e6e2006d94d5555d
Konstantin S. Uvarin <khedin@gmail.com>
2024-07-30 19:15:04 +0200

    [api] Add SKI.free(name), SKI.church(n), and SKI.[SKICBC] to access common combinators from code

---
+++ test/07-builtins.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const { SKI } = require('../index');
+
+describe( 'SKI.*', () => {
+    it('provides essential constants', done => {
+        const {S, K, I} = SKI;
+        expect( S.apply(K).apply(K).apply(I).run().result.toString() ).to.equal('I');
+
+        done();
+    });
+
+    it('provides free vars & Church numbers', done => {
+        expect (
+          SKI.church(5).apply(SKI.free('x')).apply(SKI.free('y')).run().result.toString()
+        ).to.equal('x(x(x(x(x(y)))))');
+        done();
+    });
+})
