6c9f137b6ef849ccbe96680e41e56d971726cac2
Konstantin S. Uvarin <khedin@gmail.com>
2024-08-02 21:40:34 +0200

    [api] Add SKI.parseMultiline(source, {}) to parse expressions with multiple definitions

---
+++ test/08-multiline.js
@@ -0,0 +1,21 @@
+const {expect} = require('chai');
+const {SKI} = require('../index');
+
+describe( 'SKI.pasreMulti', () => {
+    it ('handles comments, definitions, and stuff', done => {
+        const ski = new SKI;
+        const vars = {};
+        const expr = ski.parseMulti(`
+            // this is a comment
+            dbl = S (S(KS)K) K;;;;
+            dbl dbl x
+        `, vars);
+        expect( expr.run(SKI.S).result.toString() ).to.equal(expr.run().result.toString());
+        expect(Object.keys(ski.getTerms()).sort()).to.deep.equal(['I', 'K', 'S']);
+
+        expect(Object.keys(vars).sort()).to.deep.equal(['dbl', 'x']);
+        expect(vars.dbl.run(SKI.K, SKI.I).result.toString()).to.equal('K(K)');
+
+        done();
+    });
+});
