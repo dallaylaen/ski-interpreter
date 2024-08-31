d104df0fb9923bafc5d1afef433c2b157c0f4f8c
Konstantin S. Uvarin <khedin@gmail.com>
2024-08-31 13:01:17 +0200

    [bug] Allow lowercase terms in restrict() and showRestrict(), add docs

--- test/17-engine-toJSON.js
+++ test/17-engine-toJSON.js
@@ -4,8 +4,8 @@ const { SKI } = require ('../index');
 describe ('SKI.toJSON', () => {
     it ('makes data round trip', () => {
         const ski = new SKI({numbers: false});
-        ski.add('T', 'C(WK)');
-        ski.restrict('SKIT');
+        ski.add('swap', 'C(WK)');
+        ski.restrict('SKI swap');
 
         const str = JSON.stringify(ski);
 
@@ -15,7 +15,7 @@ describe ('SKI.toJSON', () => {
         expect(copy.hasNumbers).to.equal(false);
         expect(copy.showRestrict('+')).to.equal(ski.showRestrict('+'));
 
-        expect(copy.parse('T x y').run().expr + '').to.equal('y(x)');
+        expect(copy.parse('swap x y').run().expr + '').to.equal('y(x)');
 
 
     });
