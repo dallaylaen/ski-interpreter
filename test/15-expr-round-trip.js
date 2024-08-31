22ec400fcbd7a1507b01c43ba72fd1dd61438c50
Konstantin S. Uvarin <khedin@gmail.com>
2024-09-01 01:46:58 +0200

    [api] Add toString({terse: true}) to omit some parenthesis and spaces where possible

--- test/15-expr-round-trip.js
+++ test/15-expr-round-trip.js
@@ -9,19 +9,32 @@ describe('Expr <-> string', () => {
         ['free var', 'foobared'],
         ['some expr', 'SI(Kx)'],
         ['lambda with args', '(x->z->y(x))(t1)(t2)'],
+        ['more than 1 free vars', 'yadda yadda yeek']
     ];
 
     const ski = new SKI();
     for (const c of cases) {
         const [ name, source ] = c;
-        it (name, () => {
-            const jar = {}; // keep track of common free vars
-            const expr = ski.parse(source, jar);
+        it (name + ' (canonical)', () => {
+            const jar    = {}; // keep track of common free vars
+            const expr   = ski.parse(source, jar);
             const before = '' + expr;
-            const expr2 = ski.parse(before, jar);
-            const after =  '' + expr2;
+            const expr2  = ski.parse(before, jar);
+            const after  =  '' + expr2;
+
             expect(after).to.equal(before);
-            expect(expr.equals(expr2)).to.equal(true, 'expressions are equal internally');
+            expr.expect(expr2);
+        });
+
+        it (name + ' (terse)', () => {
+            const jar    = {}; // keep track of common free vars
+            const expr   = ski.parse(source, jar);
+            const before = expr.toString({terse: true});
+            const expr2  = ski.parse(before, jar);
+            const after  = expr2.toString(({terse: true}));
+
+            expect(after).to.equal(before);
+            expr.expect(expr2);
         });
     }
 });
