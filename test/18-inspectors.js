2773b350438d615642f9420d61380161309a3e9e
Konstantin S. Uvarin <khedin@gmail.com>
2024-09-07 19:56:56 +0200

    [bug] Rework \!nat as honest x(x(...(y)...)) so it can't be tricked so easily

--- test/18-inspectors.js
+++ test/18-inspectors.js
@@ -11,8 +11,21 @@ describe( 'interventions', () => {
         expect( () => coerce.run(ski.parse('S'))).to.throw(/coerce.*Church/);
 
     });
+
+    it ('cannot be fooled by sophisticated combinators', () => {
+        ski.parse('!nat (B(B(W))(B(B(C(K))))(20))').run().expr.expect(SKI.church(0));
+    });
+
+    it ('allows long computations', () => {
+        ski.parse('!nat (14 (S(S(KS)K)I) )').run().expr.expect(SKI.church(2**14));
+    });
+
     it ('rejects unknown terms', () => {
         expect( () => ski.parse('x->!foobar x') ).to.throw(/[Uu]nknown.*!foobar\b/);
+    });
+
+    it ('catches infinite loops', () => {
+        expect( () => console.log(ski.parse('!nat( S(K(SII))(K(SII)) )').run()) ).to.throw(/Church.*completed.*steps/);
     });
 
 })
