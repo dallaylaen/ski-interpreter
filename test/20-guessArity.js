d04ff0776451fd87287a799e810174c91444f4df
Konstantin S. Uvarin <khedin@gmail.com>
2024-10-30 18:51:38 +0100

    [api][engine] Add guessArity: add 'canonical' field containing equivalent lambda expr

--- test/20-guessArity.js
+++ test/20-guessArity.js
@@ -4,9 +4,9 @@ const { SKI } = require('../index');
 describe('Expr.guessArity', () => {
     const cases = [
         // proper
-        ['I', {arity: 1, proper: true, found: true}],
+        ['I', {arity: 1, proper: true, found: true}, 'x->x'],
         ['K', {arity: 2, proper: true, found: true}],
-        ['S', {arity: 3, proper: true, found: true}],
+        ['S', {arity: 3, proper: true, found: true}, 'x->y->z->x z (y z)'],
         ['SK', {arity: 2, proper: true, found: true}],
         ['CI', {arity: 2, proper: true, found: true}],
         ['x->y->x x', {arity: 2, proper: true, found: true}],
@@ -18,7 +18,7 @@ describe('Expr.guessArity', () => {
         ['x->K(xS)', {arity: 2, proper: false, found: true}],
 
         ['x', {arity: 0, proper: false, found: true}],
-        ['x->y x', {arity: 1, proper: false, found: true}],
+        ['x->y x', {arity: 1, proper: false, found: true}, 'x->y x'],
         ['By', {arity: 2, proper: false, found: true}],
 
         // infinite
@@ -28,9 +28,17 @@ describe('Expr.guessArity', () => {
 
     const ski = new SKI();
     for (const pair of cases) {
-        const [term, result] = pair;
+        const [term, expected, lambda] = pair;
         it ('handles '+term, ()=> {
-            expect(ski.parse(term).guessArity()).to.deep.equal(result);
+            const jar = {};
+            const found = ski.parse(term, jar).guessArity();
+            const canon = found.canonical;
+            delete found.canonical;
+            expect(found).to.deep.equal(expected);
+            if (found.found)
+                expect(canon).to.be.instanceof(SKI.classes.Expr);
+            if (lambda)
+                canon.expect(ski.parse(lambda, jar));
         });
     }
 });
