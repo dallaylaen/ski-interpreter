4813dd00fabb2a6afedbfce40a55110be700a82c
Konstantin S. Uvarin <khedin@gmail.com>
2025-02-23 16:17:34 +0100

    [core] Add '+' term that increments subsequent Church numeral, otherwise acts as a->b->c->b(a b c)

--- test/06-church-numerals.js
+++ test/06-church-numerals.js
@@ -20,4 +20,16 @@ describe( 'Church numbers', () => {
         expect(() => SKI.church(-2)).to.throw(/integer/);
 
     });
+
+    it ('keeps track of whether numbers are allowed', () => {
+        const allow = new SKI({numbers: true});
+        expect(allow.parse('2')).to.be.instanceof(SKI.classes.Church);
+        expect(allow.parse('+')).to.be.instanceof(SKI.classes.Native);
+    });
+    it ( 'keeps track of whether numbers are NOT allowed',() => {
+        const deny = new SKI({numbers: false});
+        expect(() => deny.parse('2')).to.throw(/Church num.*er.*s/);
+        expect(() => deny.parse('+')).to.throw(/'\+' .* restricted|Church num.*er.*s/);
+
+    });
 });
