61d6b816b985f6c6e513b76ef5101d4c030dbb4c
Konstantin S. Uvarin <khedin@gmail.com>
2025-03-18 02:25:03 +0100

    [core][api] Added getSymbols(): Map<Expr, number> that counts all atomic terms in Expr

---
+++ test/24-getSymbols.js
@@ -0,0 +1,39 @@
+const { expect } = require("chai");
+const { SKI } = require("../index");
+
+describe( "Expr.getSymbols", () => {
+  it ("handler simple exprs", () => {
+    const ski = new SKI();
+    const jar = {};
+    const expr = ski.parse("S x (y y) z", jar);
+    expect(expr.getSymbols()).to.deep.equal(new Map([[SKI.S, 1], [jar.x, 1], [jar.y, 2], [jar.z, 1]]));
+  });
+
+  it ("handles aliases", () => {
+    const ski = new SKI();
+    const jar = {};
+    const expr = ski.parse("M = SII; M x y", jar);
+    expect(expr.getSymbols()).to.deep.equal(new Map([[SKI.S, 1], [SKI.I, 2], [jar.x, 1], [jar.y, 1]]));
+  });
+
+  it ("handles nested exprs", () => {
+    const ski = new SKI();
+    const jar = {};
+    const expr = ski.parse("S x (y (z z))", jar);
+    expect(expr.getSymbols()).to.deep.equal(new Map([[SKI.S, 1], [jar.x, 1], [jar.y, 1], [jar.z, 2]]));
+  });
+
+  it ("handles lambda exprs", () => {
+    const ski = new SKI();
+    const jar = {};
+    const expr = ski.parse("x->y->y (x y)", jar);
+    expect(expr.getSymbols()).to.deep.equal(new Map());
+  });
+
+  it ("handles more lambda exprs", () => {
+    const ski = new SKI();
+    const jar = {};
+    const expr = ski.parse("x->xSK", jar);
+    expect(expr.getSymbols()).to.deep.equal(new Map([[SKI.S, 1], [SKI.K, 1]]));
+  });
+});
