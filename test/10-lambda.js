fbbe0f4d917bf585bf00c608efb932f6ef6f912c
Konstantin S. Uvarin <khedin@gmail.com>
2025-07-17 16:41:53 +0200

    [test][wip] Add 2 TODO tests for immutable parser

--- test/10-lambda.js
+++ test/10-lambda.js
@@ -101,6 +101,22 @@ describe ('Lambda parsing', () => {
   it ('forbids naughty syntax III', () => {
     expect(()=>ski.parseLine('x->y->y(x)->S')).to.throw();
   });
+
+  /*
+  // TODO: this requires parser overhaul - make everything immutable
+  it ('doesn\'t pollute the vars hash with intermittent vars', () => {
+    const jar = {};
+    const expr = ski.parseLine('x(y->z)', jar);
+    expect(Object.keys(jar).sort()).to.deep.equal(['x', 'z']);
+  });
+
+  it ('doesn\'t pollute the vars hash with intermittent vars II', () => {
+    const jar = {};
+    const expr = ski.parseLine('(x->y->x x)x', jar);
+    expect(expr.run(SKI.I).expr.toString()).to.equal('x x');
+    expect(Object.keys(jar).sort()).to.deep.equal(['x']);
+  });
+  */
 });
 
 describe ('more lambdas', () =>{
