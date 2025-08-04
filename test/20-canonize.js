ed3b29a128eb7595c2b3105723fee04ee19f6a15
Konstantin S. Uvarin <khedin@gmail.com>
2025-08-04 02:37:24 +0200

    [core][wip] Rewrite canonize() so that it always produces max lambda, + add {duplicate: boolean, discard: boolean} which tell whether _any_ term in canonical expression is capable of respective action

--- test/20-canonize.js
+++ test/20-canonize.js
@@ -4,33 +4,41 @@ const { SKI } = require('../index');
 describe('Expr.canonize', () => {
   const cases = [
     // proper
-    ['I',           {arity: 1, proper: true, linear: true,  found: true}, 'x->x'],
-    ['K',           {arity: 2, proper: true, linear: false, found: true, skip: new Set([1])}],
-    ['S',           {arity: 3, proper: true, linear: false, found: true, dup: new Set([2])}, 'x->y->z->x z (y z)'],
-    ['SK',          {arity: 2, proper: true, linear: false, found: true, skip: new Set([0])}],
-    ['CI',          {arity: 2, proper: true, linear: true,  found: true}, 'x->y->y x'],
-    ['x->y->x x',   {arity: 2, proper: true, linear: false, found: true, skip: new Set([1]), dup: new Set([0])}, 'x->y->x x'],
-    ['5',           {arity: 2, proper: true, linear: false, found: true, dup: new Set([0])}, 'x->y->x(x(x(x(x y))))'],
+    ['I',           {found: true, arity: 1, proper: true, linear: true,  discard: false, duplicate: false, }, 'x->x'],
+    ['K',           {found: true, arity: 2, proper: true, linear: false, discard: true , duplicate: false, skip: new Set([1])}],
+    ['S',           {found: true, arity: 3, proper: true, linear: false, discard: false, duplicate: true , dup: new Set([2])}, 'x->y->z->x z (y z)'],
+    ['SK',          {found: true, arity: 2, proper: true, linear: false, discard: true , duplicate: false, skip: new Set([0])}],
+    ['CI',          {found: true, arity: 2, proper: true, linear: true,  discard: false, duplicate: false, }, 'x->y->y x'],
+    ['x->y->x x',   {found: true, arity: 2, proper: true, linear: false, discard: true , duplicate: true , skip: new Set([1]), dup: new Set([0])}, 'x->y->x x'],
+    ['5',           {found: true, arity: 2, proper: true, linear: false, discard: false, duplicate: true, dup: new Set([0])}, 'x->y->x(x(x(x(x y))))'],
 
     // improper
-    ['CIS',         {arity: 1, proper: false, linear: false, found: true}],
-    ['x->xSK',      {arity: 1, proper: false, linear: false, found: true}],
+    ['CIS',         {found: true, arity: 1, proper: false, linear: false, discard: false, duplicate: true, }, 'x->x(a->b->c->a c (b c))'],
+    ['x->xSK',      {found: true, arity: 1, proper: false, linear: false, discard: true,  duplicate: true}, 'x->x(a->b->c->a c (b c))(a->b->a)'],
     ['x->x(a->b->c->a c (b c))(a->b->a)',
-            {arity: 1, proper: false, linear: false, found: true}],
-    ['x->K(xS)',    {arity: 2, proper: false, linear: false, found: true, skip: new Set([1])}],
+            {arity: 1, proper: false, linear: false, found: true, discard: true,  duplicate: true}],
+    ['x->K(xS)',    {arity: 2, proper: false, linear: false, found: true, discard: true,  duplicate: true, skip: new Set([1])},
+                    'x->y->x(a->b->c->a c (b c))'],
 
-    ['x',           {arity: 0, proper: false, linear: false, found: true}],
-    ['x->y x',      {arity: 1, proper: false, linear: false, found: true}, 'x->y x'],
-    ['By',          {arity: 2, proper: false, linear: false, found: true}],
+    ['x',           {found: true, arity: 0, proper: false, linear: false, discard: false, duplicate: false, }, 'x'],
+    ['x->y x',      {found: true, arity: 1, proper: false, linear: false, discard: false, duplicate: false, }, 'x->y x'],
+    ['By',          {found: true, arity: 2, proper: false, linear: false, discard: false, duplicate: false, }, 'a->b->y(a b)'],
 
     // infinite recursion
-    ['SII(SII)',    {proper: false, found: false}, '(x->x x)(x->x x)'],
+    ['SII(SII)',    {found: false, arity: 0, proper: false, linear: false, discard: false, duplicate: true}, '(x->x x)(x->x x)'],
+    ['M=WI; MM',    {found: false, arity: 0, proper: false, linear: false, discard: false, duplicate: true}, '(x->x x)(x->x x)'],
+    ['M=WI; x->MM',   {found: false, arity: 1, proper: false, linear: false, discard: true, duplicate: true, skip: new Set([0])}, 'x->(y->y y)(y->y y)'],
+    // Y combinator
+    ['Y=WI(W(B(SI)))', {found: false, arity: 0, proper: false, linear: false, discard: false, duplicate: true}],
     // quine eats all args
-    ['WI(SBK)',     {proper: false, found: false}, '(x->x x) (x->y->x x)'],
+    ['WI(SBK)',     {found: false, arity: 0, proper: false, linear: false, discard: true, duplicate: true}, '(x->x x) (x->y->x x)'],
 
     // hidden by an alias
-    ['P=a->b->c->d->b(a d c); P',       {proper: true, found: true, linear: true,  arity: 4}, 'a->b->c->d->b(a d c)'],
-    ['P=a->b->c->d->b(a d c); P(PII)',  {proper: true, found: true, linear: true,  arity: 3}, 'a->b->c->a(b c)'],
+    ['P=a->b->c->d->b(a d c); P',       {found: true, proper: true, linear: true, discard: false, duplicate: false, arity: 4}, 'a->b->c->d->b(a d c)'],
+    ['P=a->b->c->d->b(a d c); P(PII)',  {found: true, proper: true, linear: true, discard: false, duplicate: false, arity: 3}, 'a->b->c->a(b c)'],
+
+    // lambda maps to itself
+    ['x->y->z->x z y', {found: true, arity: 3, proper: true, linear: true, discard: false, duplicate: false, }, 'x->y->z->x z y'],
   ];
 
   const ski = new SKI();
@@ -50,11 +58,13 @@ describe('Expr.canonize', () => {
         canon.expect(ski.parse(lambda, jar));
       expect(canon).to.be.instanceof(SKI.classes.Expr);
       expect(steps).to.be.a('number');
-      expect(steps).to.be.within(
-        0,
-        SKI.options.max * (found.arity ?? SKI.options.maxArgs),
-        'steps should be less than max * arity'
-      );
+      if (found.found) {
+        expect(steps).to.be.within(
+          0,
+          SKI.options.max * (found.arity ?? SKI.options.maxArgs),
+          'steps should be less than max * arity'
+        );
+      }
     });
   }
 });
