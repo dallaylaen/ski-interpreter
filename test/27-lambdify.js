a5ea02f75fbed7b2524451e5f470568a431ae9e0
Konstantin S. Uvarin <khedin@gmail.com>
2025-08-14 11:48:37 +0200

    [test] Rewrite lambdify test

--- test/27-lambdify.js
+++ test/27-lambdify.js
@@ -1,82 +1,94 @@
 const { expect } = require('chai');
 const { SKI } = require('../index');
 
+const ski = new SKI();
+
 describe('Expr.lambdify', () => {
-  const ski = new SKI();
 
-  const cases = [
     // basic types
-    ["x", "x"],
-    ["K x y", "x"],
-    ["S", "x->y->z->x z (y z)"],
-    ["3", "x->y->x(x(x y))"],
-    ["a->b->b a", "x->y->y x"],
+    checkTerm("x", "x");
+    checkTerm("K x y", "x");
+    checkTerm("S", "x->y->z->x z (y z)");
+    checkTerm("3", "x->y->x(x(x y))");
+    checkTerm("a->b->b a", "x->y->y x");
 
-    ["x->xSK", "x->x (a->b->c->a c (b c)) (a->b->a)" ],
-    ["X=x->xSK; X(X(X(XX)))", "a->b->c->a c (b c)"],
-    ["M=SII; MM", "(x->x x) (x->x x)"],
-    ["BC(CI)", "a->b->c->c a b"],
-    ["T=CI; 5 (Ty) x", "x y y y y y"],
+    checkTerm("x->xSK", "x->x (a->b->c->a c (b c)) (a->b->a)" );
+    checkTerm("X=x->xSK; X(X(X(XX)))", "a->b->c->a c (b c)");
+    checkTerm("M=SII; MM", "(x->x x) (x->x x)");
+    checkTerm("BC(CI)", "a->b->c->c a b");
+    checkTerm("T=CI; 5 (Ty) x", "x y y y y y");
 
     // quine
-    ["SII(C(K(WI)))", "(x->x x) (x->y->x x)"],
+    checkTerm("SII(C(K(WI)))", "(x->x x) (x->y->x x)");
 
     // Y combinator
-    ["WI(W(B(SI)))", "(x->x x)(g->f->f(g g f))"],
+    checkTerm("WI(W(B(SI)))", "(x->x x)(g->f->f(g g f))");
 
     // SS(KI) decoded as W
-    ["(a->b->c->a c (b c)) (a->b->c->a c (b c)) (a->b->b)", "x->y->x y y"],
+    checkTerm("(a->b->c->a c (b c)) (a->b->c->a c (b c)) (a->b->b)", "x->y->x y y");
 
-    ["WWW", "(x->x x x)(x->y->x y y)"],
+    checkTerm("WWW", "(x->x x x)(x->y->x y y)");
 
     // this is f such that f x = f x x (so it generates infinite string of x's)
-    ["WI(BW(BW (C(K(WI))))) ", "(f->f f)(g->x->g g x x)"],
-    ["BW(BW (C(K(WI))))", "g->x->g g x x"],
+    checkTerm("WI(BW(BW (C(K(WI))))) ", "(f->f f)(g->x->g g x x)");
+    checkTerm("BW(BW (C(K(WI))))", "g->x->g g x x");
 
-    ["BS(C(BB))", "a->b->c->d->c a(b c d)"],
-  ];
-
-  for (const [term, final] of cases) {
-    it(`evaluates ${term} to ${final}`, () => {
-      const jar = {};
-      const expr = ski.parse(term, jar);
-      const seq = expr.lambdify({latin: 6, maxArgs: 10});
-      const expected = ski.parse(final, jar);
-
-      let done = false;
-      let i = 0;
-      let steps = 0;
-      let weight = Infinity;
-      const trace = [];
-      for (const step of seq) {
-        trace.push(step.expr);
-        expect(done).to.equal(false, 'we didn\'t iterate past the final step');
-        expect(i++).to.be.lessThanOrEqual(30, 'no runaway sequences');
-
-        expect(step.steps).to.be.a('number');
-        expect(step.steps).to.be.greaterThanOrEqual(steps);
-        steps = step.steps;
-
-        const expr = step.expr;
-        expect(expr).to.be.instanceOf(SKI.classes.Expr);
-        expect(expr.weight()).to.be.lessThan(weight, 'term weight must strictly diminish');
-        weight = expr.weight();
-
-        console.log('[' + step.steps + '] ' + expr.toString({terse:true}) + ' // ' + step.comment + '; weight: ' + expr.weight());
-
-        const sym = expr.getSymbols();
-        const nonFree = [...sym.keys()].filter(s => !(s instanceof SKI.classes.FreeVar) && s !== SKI.classes.Expr.lambdaPlaceholder);
-        expect(nonFree).to.be.deep.equal([], 'expr must only have free variables and ->');
-
-        // don't do fancy variables unless explicitly told to
-        ski.parse('' + expr, jar); // TODO .expect(step.expr);
-
-        if (expected.equals(expr)) {
-          done = true;
-        }
-      }
-
-      expect(done).to.equal(true, 'we reached ' + expected + ' in the final step of '+trace.join('; '));
-    });
-  }
+    checkTerm("BS(C(BB))", "a->b->c->d->c a(b c d)");
 });
+
+function checkTerm(startSrc, endSrc, options = {}) {
+  describe (`Expr.lambdify ${startSrc} -> ${endSrc}`, () => {
+    try {
+      const jar = {};
+      const start = ski.parse(startSrc, jar);
+      const end = ski.parse(endSrc, jar);
+      const seq = start.lambdify(options);
+      let weight = Infinity;
+      let steps = 0;
+      let expr;
+      let finished = false;
+      for (let i = 0; i < 30; i++) {
+        const {value, done} = seq.next();
+        if (done) {
+          finished = true;
+          break;
+        }
+        expr = value.expr;
+        it ('produces a smaller expression every time: '+expr, () =>{
+          expect(expr).to.be.instanceOf(SKI.classes.Expr);
+          expect(expr.weight()).to.be.lessThanOrEqual(weight, 'term weight must strictly diminish');
+          weight = expr.weight();
+          expect(value.steps).to.be.greaterThanOrEqual(steps, 'steps must not decrease');
+          steps = value.steps;
+
+          const sym = expr.getSymbols();
+          const nonFree = [...sym.keys()].filter(s => !(s instanceof SKI.classes.FreeVar) && s !== SKI.classes.Expr.lambdaPlaceholder);
+          expect(nonFree).to.be.deep.equal([], 'expr must only have free variables and ->');
+
+          expr.expect(ski.parse(''+expr, jar), 'expression parses to itself when stringified');
+        });
+      }
+      it ('terminates', () => {
+        expect(finished).to.equal(true, 'did not finish in 30 steps');
+      });
+      it ('is idempotent', () => {
+        expect(expr).to.be.instanceOf(SKI.classes.Expr);
+        expr.expect(drain(expr.lambdify(options)).expr);
+      });
+    }
+    catch (e) {
+      it ("doesn't die", () => {
+        throw e;
+      });
+    }
+  });
+
+}
+
+function drain(seq) {
+  let result;
+  for (const step of seq) {
+    result = step;
+  }
+  return result;
+}
