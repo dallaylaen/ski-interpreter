53d44de9c0097f8d61929fab042fe39a283dff13
Konstantin Uvarin <khedin@gmail.com>
2025-12-11 00:43:40 +0100

    [test] Improve format() coverage

--- test/32-format.js
+++ test/32-format.js
@@ -4,7 +4,7 @@ const { SKI } = require('../index');
 
 const ski = new SKI();
 
-describe('Expr.format', () => {
+describe('Expr.format: literal', () => {
   check('SKK', {}, 'SKK');
   check('foo bar', {}, 'foo bar');
   check('foo bar quux', {}, 'foo bar quux');
@@ -14,6 +14,25 @@ describe('Expr.format', () => {
   check('IIx', {html: true}, '<b>I</b>I<var>x</var>');
   check('a->a a', {html: true}, '<var>a</var>-&gt;<var>a</var> <var>a</var>');
   check('(x->y->y x)(foo)', {}, '(x->y->y x)foo');
+  check('K 2 S', {}, 'K 2 S');
+  check('5 x y', {}, '5 x y');
+  check('5 SK', {}, '5 SK');
+});
+
+describe('Expr.format: round-trip', () => {
+  roundTrip('foo');
+  roundTrip('foo bar');
+  roundTrip('foo (bar baz)');
+  roundTrip('(foo bar) baz');
+
+  roundTrip('S K K');
+  roundTrip('I x');
+  roundTrip('x->x');
+  roundTrip('(x->x x)(x->x x)');
+
+  roundTrip('S(KS)K');
+  roundTrip('x->y->z->x z (y z)');
+  roundTrip('x(y(z t))')
 });
 
 function check (src, options, result, comment) {
@@ -34,5 +53,51 @@ function check (src, options, result, comment) {
     });
 
 
+  });
+}
+
+function roundTrip (src) {
+  describe(src + ' round trips', () => {
+    const jar = {};
+    const expr = ski.parse(src, jar);
+
+    it ('format w/o options coincides with toString', () => {
+      expect(expr.format()).to.equal(expr.toString());
+    });
+
+    sameAs(
+      expr.format({}),
+      jar,
+      expr,
+      'default options'
+    );
+
+    sameAs(
+      expr.format({terse: false}),
+      jar,
+      expr,
+      'terse: false'
+    );
+
+    it ('round-trips with html:true', () => {
+      const formatted = expr.format({html: true});
+      const stripped = formatted.replace(/<[^>]+>/g, '').replace(/&gt;/g, '>');
+      const expr2 = ski.parse(stripped, jar);
+      expr.expect(expr2);
+    });
+
+    sameAs(
+      expr.format({ around: ['(', ')'], brackets: ['', ''], lambda: ['(', '->', ')'] }),
+      jar,
+      expr,
+      'lisp style: braces around application, not arguments'
+    );
+  });
+}
+
+function sameAs (src, jar, expr, comment) {
+  it (`${comment}: evaluates ${src} as ${expr}`, () => {
+    const expr2 = ski.parse(src, jar);
+    expr.expect(expr2, comment);
   });
 }
