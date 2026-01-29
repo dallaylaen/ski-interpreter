61c5474a7ce1ab9080769760f9f2e9c48fa56b0d
Konstantin Uvarin <khedin@gmail.com>
2026-01-30 00:43:47 +0100

    [wip][core] More test cases, add some docs to diff & equals

--- test/35-diff.js
+++ test/35-diff.js
@@ -14,6 +14,23 @@ describe('Expr.diff', () => {
     const diff = expr1.diff(expr2); // string
 
     expect(diff).to.match(/^S\(K\(/, 'contains common prefix');
-    expect(diff).to.match(/foo *!= bar/, 'shows differing part in correct order');
+    expect(diff).to.match(/foo *!= *bar/, 'shows differing part in correct order');
+
+    expect(expr1.diff(expr2, true)).to.match(/bar *!= *foo/, 'reversed order when requested');
+  });
+
+  it ('shows when expressions are identical', () => {
+    const ski = new SKI();
+
+    const expr1 = ski.parse('S(KS)K');
+    const expr2 = ski.parse('S(KS)K');
+
+    expect(expr1.diff(expr2)).to.equal(null);
+  });
+
+  it ('handles aliases correctly', () => {
+    const ski = new SKI();
+
+
   });
 });
