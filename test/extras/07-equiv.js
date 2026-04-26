6fd27813625646e617abd0fa133a5918dc38a79f
Konstantin Uvarin <khedin@gmail.com>
2026-04-26 12:42:35 +0200

    [test] SKI.extras.equiv() test updated

--- test/extras/07-equiv.js
+++ test/extras/07-equiv.js
@@ -31,8 +31,28 @@ function isEquiv (message, src1, src2, options) {
     const expr1 = ski.parse(src1);
     const expr2 = ski.parse(src2);
     const res = SKI.extras.equiv(expr1, expr2, options);
+    it('produces sensible result', () => {
+      expect(res).to.be.an('object');
+      expect(res.equal).to.be.a('boolean');
+      expect(res.normal).to.be.a('boolean');
+      expect(res.steps).to.be.a('number');
+      expect(res.steps).to.be.within(0, 100000);
+      expect(res.canonical).to.be.an('array');
+      expect(res.canonical.length).to.equal(2);
+    });
     it(equal ? 'equal' : 'not equal', () => {
       expect(res.equal).to.equal(equal);
     });
+    if (equal) {
+      it('is normal', () => {
+        expect(res.normal).to.equal(true);
+      });
+    }
+    if (res.normal) {
+      it('has canonical forms', () => {
+        expect(res.canonical[0]).to.be.instanceof(SKI.classes.Expr);
+        expect(res.canonical[1]).to.be.instanceof(SKI.classes.Expr);
+      });
+    }
   });
 }
