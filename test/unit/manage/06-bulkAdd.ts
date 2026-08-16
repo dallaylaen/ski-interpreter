a0da109e005f2bb2b542855802d6c9bc1e594ae9
Konstantin Uvarin <khedin@gmail.com>
2026-08-16 12:39:50 +0200

    [test] Add bulk-add test for atomic

--- test/unit/manage/06-bulkAdd.ts
+++ test/unit/manage/06-bulkAdd.ts
@@ -1,5 +1,7 @@
 import { expect } from 'chai';
 import { SKI } from '../../../src';
+import { isInstanceOf } from '../../lib/assert';
+import { PureNative } from '../../../src/expr';
 
 describe('Parser.bulkAdd', () => {
   it('can set up terms', () => {
@@ -22,5 +24,16 @@ describe('Parser.bulkAdd', () => {
     expect(ski.getTerms().V).to.be.instanceof(SKI.classes.Alias);
 
     ski.parse('c a b').expect(ski.parse('V a b c').run().expr);
+  });
+
+  it('can import @atomic terms', () => {
+    const ski = new SKI({ atomic: true });
+    ski.bulkAdd(['@atomic V=a->b->f->f a b']);
+
+    isInstanceOf(ski.getTerms().V, PureNative);
+
+    const trace = [...ski.parse('V a b f').walk()].map(x => x.expr + '');
+
+    expect(trace.map(s => s.replace(/ /g, ''))).to.deep.equal(['Vabf', 'fab']);
   });
 });
