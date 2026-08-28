1ec87a2053226cfe3b22471adb82e3e9810a7d82
Konstantin Uvarin <khedin@gmail.com>
2026-08-28 21:04:49 +0200

    [test] Add more modify-declare-bulkAdd round-trip tests

--- test/unit/manage/08-declare-mutation.ts
+++ test/unit/manage/08-declare-mutation.ts
@@ -1,18 +1,39 @@
 import { expect } from 'chai';
-import { SKI } from '../../../src/index';
+import { SKI } from '../../../src';
 import { isInstanceOf } from '../../lib/assert';
 import { Named } from '../../../src/expr';
 
 describe('SKI: adding terms to an engine, step by step', () => {
-  const ski = new SKI({ atomic: true });
-  modifyEngine(ski, 'nil=KI', ['nil x y']);
-  modifyEngine(ski, 'T=CI', ['T x y']);
-  modifyEngine(ski, 'V=BCT', ['V a b c']);
-  modifyEngine(ski, '@atomic M=a->a a', ['M x y']);
-  modifyEngine(ski, 'T', ['V a b c'], true);
+  describe('easy case, minimal interdependencies', () => {
+    const ski = new SKI({ atomic: true });
+    modifyEngine(ski, 'nil=KI', [['nil x y', 'y']]);
+    modifyEngine(ski, 'T=CI', [['T x y', 'y x']]);
+    modifyEngine(ski, 'V=BCT', [['V a b c', 'c a b']]);
+    modifyEngine(ski, '@atomic M=a->a a', [['M x', 'x x']]);
+    modifyEngine(ski, 'T', [['V a b c', 'c a b'], ['T a b', 'T=; T a b']], true);
+  });
+
+  describe('override primitive term', () => {
+    const ski = new SKI({ atomic: true });
+    modifyEngine(ski, 'T=S(K(SI))K', [['T x y', 'y x']]);
+    modifyEngine(ski, 'M=SII', [['M x', 'x x']]);
+    modifyEngine(ski, 'tmp = S', []);
+    modifyEngine(ski, 'S=K', []);
+    modifyEngine(ski, 'K=tmp', []);
+    modifyEngine(ski, 'tmp', [], true);
+    modifyEngine(ski, 'B=K(SK)S', [['B x y z', 'x (y z)']]);
+    modifyEngine(ski, 'R=MBT', [['R x y z', 'y z x']]);
+  });
+
+  describe('compound atomic terms', () => {
+    const ski = new SKI({ atomic: true });
+    modifyEngine(ski, '@atomic P = x->y->x', [['P a b', 'a']]);
+    modifyEngine(ski, '@atomic Q = x->y->z->x z (y z)', [['Q a b c', 'a c (b c)'], ['QPPa', 'a']]);
+    modifyEngine(ski, '@atomic X = x->xQP', [['X(XX) a b', 'b']]);
+  });
 });
 
-function modifyEngine (engine: SKI, modification: string, sample: string[], remove: boolean = false) {
+function modifyEngine (engine: SKI, modification: string, sample: [src: string, result: string][], remove: boolean = false) {
   const comment = `after ${remove ? 'removing' : 'adding'} ${modification}`;
 
   describe(comment, () => {
@@ -28,7 +49,7 @@ function modifyEngine (engine: SKI, modification: string, sample: string[], remo
       }
 
       const decl = engine.declare();
-      console.log(comment, decl);
+      // console.log(comment, decl);
       copy = new SKI({ experimental: true });
       copy.bulkAdd(decl);
     });
@@ -47,11 +68,14 @@ function modifyEngine (engine: SKI, modification: string, sample: string[], remo
       }
     });
 
-    for (const src of sample) {
-      it(`sample ${src} should parse to equal expressions`, () => {
+    for (const [src, toSrc] of sample) {
+      it(`sample '${src}' should be equal and reduce to '${toSrc}'`, () => {
         const expr1 = engine.parse(src);
         const expr2 = copy.parse(src);
+        const result = engine.parse(toSrc);
         expr1.expect(expr2);
+        result.expect(expr1.run().expr);
+        result.expect(expr2.run().expr);
       });
     }
   });
